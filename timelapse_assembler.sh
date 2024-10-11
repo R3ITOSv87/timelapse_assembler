9b834f8b5b53b016dfba9af6f8a9fde7f98b0fe7
R3ITOSv87 <christoph@reit-it.de>
2024-10-11 17:46:48 +0200

    Update timelapse_assembler.sh
    Added Stabilization

--- timelapse_assembler.sh
+++ timelapse_assembler.sh
@@ -14,6 +14,7 @@ function get_variable() {
 
 # Process arguments
 REMOVE_DARK=false
+STABILIZE=false
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -d|--directory)
@@ -32,6 +33,10 @@ while [[ "$#" -gt 0 ]]; do
             REMOVE_DARK=true
             shift 1
             ;;
+        --stabilize)
+            STABILIZE=true
+            shift 1
+            ;;
         *)
             echo "Error: Unknown option '$1'. Use -h or --help for usage information."
             exit 1
@@ -45,11 +50,19 @@ OUTPUT_FILE=$(get_variable "OUTPUT_FILE" "Enter the output file name" "timelapse
 FPS=$(get_variable "FPS" "Enter frames per second (FPS)" "30")
 
 # Ask interactively if dark images should be removed
-REMOVE_DARK_INPUT=$(get_variable "REMOVE_DARK" "Do you want to remove dark images? (yes/no)" "no")
+REMOVE_DARK_INPUT=$(get_variable "REMOVE_DARK" "Do you want to remove dark images? (yes/no)" "yes")
 if [[ "$REMOVE_DARK_INPUT" == "yes" ]]; then
     REMOVE_DARK=true
 else
     REMOVE_DARK=false
+fi
+
+# Ask interactively if stabilization should be applied
+STABILIZE_INPUT=$(get_variable "STABILIZE" "Do you want to stabilize the timelapse? (yes/no)" "yes")
+if [[ "$STABILIZE_INPUT" == "yes" ]]; then
+    STABILIZE=true
+else
+    STABILIZE=false
 fi
 
 # Check if the specified directory exists
@@ -76,10 +89,21 @@ if [ "$REMOVE_DARK" = true ]; then
 fi
 
 # Use ffmpeg to create a timelapse
-ffmpeg -pattern_type glob -i "$DIRECTORY/*.jpg" -vf "fps=$FPS" -pix_fmt yuv420p "$OUTPUT_FILE" || {
+ffmpeg -pattern_type glob -i "$DIRECTORY/*.jpg" -vf "fps=$FPS" -pix_fmt yuvj420p "$OUTPUT_FILE" || {
     echo "Error creating timelapse."
     exit 1
 }
+
+# Stabilize the timelapse if requested
+if [ "$STABILIZE" = true ]; then
+    echo "Stabilizing the timelapse..."
+    ffmpeg -i "$OUTPUT_FILE" -vf vidstabdetect=shakiness=10:accuracy=15:result="transforms.trf" -f null -
+    ffmpeg -i "$OUTPUT_FILE" -vf vidstabtransform=smoothing=30:input="transforms.trf" -c:v libx264 -preset slow -crf 18 "$OUTPUT_FILE"_stabilized.mp4 || {
+        echo "Error stabilizing timelapse."
+        exit 1
+    }
+    mv "$OUTPUT_FILE"_stabilized.mp4 "$OUTPUT_FILE"
+fi
 
 # Clean up temporary directory if used
 if [ "$REMOVE_DARK" = true ]; then
